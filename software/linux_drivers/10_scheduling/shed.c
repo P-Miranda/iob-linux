1a20c6d14ab1fe148c660c58504964bcf8fcd759
PMiranda <pmiranda.iobundle@gmail.com>
2024-01-16 18:52:35 +0000

    feat(drivers): schedule tasks/work queues

---
+++ software/linux_drivers/10_scheduling/shed.c
@@ -0,0 +1,30 @@
+/*
+ * sched.c
+ * 1. add module to kernel: insmod shed.ko
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/workqueue.h>
+
+static struct workqueue_struct *queue = NULL;
+static struct work_struct work;
+
+static void work_handler(struct work_struct *data) {
+  pr_info("work handler function.\n");
+}
+
+static int __init sched_init(void) {
+  queue = alloc_workqueue("HELLOWORLD", WQ_UNBOUND, 1);
+  INIT_WORK(&work, work_handler);
+  queue_work(queue, &work);
+  return 0;
+}
+
+static void __exit sched_exit(void) { destroy_workqueue(queue); }
+
+module_init(sched_init);
+module_exit(sched_exit);
+
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_AUTHOR("IObundle");
+MODULE_DESCRIPTION("Workqueue example");
